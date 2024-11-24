cc371a36d368f126265b57d98a41c2e9393c334f
wardennkoil <109043302+wardennkoil@users.noreply.github.com>
2024-11-24 18:57:32 -0500

    replaced divs with views

--- app/components/RestaurantDetails.tsx
+++ app/components/RestaurantDetails.tsx
@@ -6,7 +6,6 @@ import restaurantsData from "../data/restaurants.json";
 import { useLocalSearchParams } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
 
-
 const RestaurantDetails = () => {
   const router = useRouter();
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -14,98 +13,92 @@ const RestaurantDetails = () => {
 
   const toggleMenu = () => setMenuVisible(!menuVisible);
 
-
   const restaurant = restaurantsData.find((item) => item.id === id);
   if (!restaurant) {
     return (
-      <View style={g_style.container}>
-        <Text style={g_style.title}>Not Found</Text>
-      </View>
+        <View style={g_style.container}>
+          <Text style={g_style.title}>Not Found</Text>
+        </View>
     );
   } else {
     return (
+        <View style={g_style.container}>
+          <View style={style.row}>
 
-      <View style={g_style.container}>
-        <div style={style.row}>
-
-          <Text style={style.restaurantName}>{restaurant.name} | {restaurant.rating} ⭐</Text>
-          <TouchableOpacity onPress={toggleMenu} style={g_style.dotMenuButton}>
-            <Text style={g_style.dots}>⋮</Text>
-          </TouchableOpacity>
-          <Modal
-            transparent
-            animationType="fade"
-            visible={menuVisible}
-            onRequestClose={toggleMenu}
-          >
-            <TouchableOpacity style={g_style.overlay} onPress={toggleMenu}>
-              <View style={g_style.dotmenu}>
-                <TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log("Route: Show on Map")}
-                >
-                  <Text style={g_style.menuText}>Show on Map</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log("Route: Get Directions")}
-                >
-                  <Text style={g_style.menuText}>Get Directions</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log("Route: Edit")}
-                >
-                  <Text style={g_style.menuText}>Edit</Text>
-                </TouchableOpacity><TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log("Route: Share")}
-                >
-                  <Text style={g_style.menuText}>Share</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log("Delete")}
-                >
-                  <Text style={g_style.menuText}>Delete</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={g_style.dotmenuItem}
-                  onPress={() => console.log()}
-                >
-                  <Text style={g_style.menuText}></Text>
-                </TouchableOpacity>
-
-              </View>
+            <Text style={style.restaurantName}>{restaurant.name} | {restaurant.rating} ⭐</Text>
+            <TouchableOpacity onPress={toggleMenu} style={g_style.dotMenuButton}>
+              <Text style={g_style.dots}>⋮</Text>
             </TouchableOpacity>
-          </Modal>
-        </div>
+            <Modal
+                transparent
+                animationType="fade"
+                visible={menuVisible}
+                onRequestClose={toggleMenu}
+            >
+              <TouchableOpacity style={g_style.overlay} onPress={toggleMenu}>
+                <View style={g_style.dotmenu}>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log("Route: Show on Map")}
+                  >
+                    <Text style={g_style.menuText}>Show on Map</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log("Route: Get Directions")}
+                  >
+                    <Text style={g_style.menuText}>Get Directions</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log("Route: Edit")}
+                  >
+                    <Text style={g_style.menuText}>Edit</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log("Route: Share")}
+                  >
+                    <Text style={g_style.menuText}>Share</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log("Delete")}
+                  >
+                    <Text style={g_style.menuText}>Delete</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={g_style.dotmenuItem}
+                      onPress={() => console.log()}
+                  >
+                    <Text style={g_style.menuText}></Text>
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            </Modal>
+          </View>
 
+          <Text style={style.restaurantAddress}>{restaurant.address}</Text>
 
-        <Text style={style.restaurantAddress}>{restaurant.address}</Text>
+          <View style={style.row}>
+            {restaurant.tags.map((tag, index) => (
+                <TouchableOpacity key={index} onPress={() => console.log(tag)}>
+                  <Text style={style.restaurantTag}>{tag}</Text>
+                </TouchableOpacity>
+            ))}
+          </View>
 
-
-
-        <div style={style.row}>
-          {restaurant.tags.map((tag, index) => (
-            <TouchableOpacity key={index} onPress={() => console.log(tag)} ><Text style={style.restaurantTag}>{tag}</Text></TouchableOpacity>
+          <Text style={style.subHeader}>Phones:</Text>
+          {restaurant.phones.map((phone, index) => (
+              <Text key={index} style={style.restaurantPhone}>{phone}</Text>
           ))}
-        </div>
 
-        <Text style={style.subHeader}>Phones:</Text>
-        {restaurant.phones.map((phone, index) => (
-          <Text key={index} style={style.restaurantPhone}>{phone}</Text>
-        ))}
+          <Text style={style.subHeader}>Description:</Text>
+          <Text style={style.restaurantDescription}>{restaurant.description}</Text>
 
-        <Text style={style.subHeader}>Description:</Text>
-        <Text style={style.restaurantDescription}>{restaurant.description}</Text>
-
-      </View>
+        </View>
     )
   }
 }
-
-
-
 
 export default RestaurantDetails;
--- app/components/RestaurantList.tsx
+++ app/components/RestaurantList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {View, Text, FlatList, StyleSheet, Modal, TouchableOpacity, Button} from "react-native";
+import { View, Text, FlatList, StyleSheet, Modal, TouchableOpacity, Button } from "react-native";
 import { useRouter } from "expo-router";
 
 import { Restaurant } from "../models/Restaurant";
@@ -7,33 +7,39 @@ import { GlobalStyles as g_style, ListStyles as style } from "../style/Styles";
 import restaurantsData from "../data/restaurants.json";
 
 const List = () => {
-    const router = useRouter()
-    const renderRestaurant = ({ item }: { item: Restaurant }) => (
-        <TouchableOpacity style={style.listItem} onPress={() =>  {
-          console.log(`Selected item ${item.id} : ${item.name}, routing to details`)
-          router.push(`./components/RestaurantDetails?id=${item.id}`);
-          }}>
-          <Text style={style.name}>{item.name}</Text>
-          <Text style={style.rating}>Rating: {item.rating.toFixed(1)}</Text>
-          <Text style={style.tags}>
-            {item.tags.join(", ")}
-          </Text>
-        </TouchableOpacity>
-      );
-    
-      return (
-        <View style={style.container}>
-          <div style={g_style.row}>
-            <Button title={"+ Add restaurant"} onPress={() => router.push("./components/AddRestaurantPage")} />
+    const router = useRouter();
 
-          </div>
-          <FlatList
-            data={restaurantsData}
-            keyExtractor={(item) => item.id}
-            renderItem={renderRestaurant}
-            ListEmptyComponent={<Text style={style.emptyText}>No restaurants found.</Text>}
-          />
+    const renderRestaurant = ({ item }: { item: Restaurant }) => (
+        <TouchableOpacity
+            style={style.listItem}
+            onPress={() => {
+                console.log(`Selected item ${item.id} : ${item.name}, routing to details`);
+                router.push(`./components/RestaurantDetails?id=${item.id}`);
+            }}
+        >
+            <Text style={style.name}>{item.name}</Text>
+            <Text style={style.rating}>Rating: {item.rating.toFixed(1)}</Text>
+            <Text style={style.tags}>{item.tags.join(", ")}</Text>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={style.container}>
+            <View style={g_style.row}>
+                <Button
+                    title="+ Add restaurant"
+                    onPress={() => router.push("./components/AddRestaurantPage")}
+                />
+            </View>
+
+            <FlatList
+                data={restaurantsData}
+                keyExtractor={(item) => item.id}
+                renderItem={renderRestaurant}
+                ListEmptyComponent={<Text style={style.emptyText}>No restaurants found.</Text>}
+            />
         </View>
     );
-}
-export default List
+};
+
+export default List;
